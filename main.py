6f28fc2286c624e47e88578da216675f5cb88e48
Valery Oficerov <86911486+Oficerov@users.noreply.github.com>
2022-02-16 15:26:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,42 @@
-import random
+from random import randint
 
 class Game():
 
-    def initialize(self):
-        self.random_value = random.randint(1, 10)
+    def __init__(self):
+        self.random_value = randint(1, 10)
         print(self.random_value) #убрать перед игрой
         self.basic_validation()
-
-    def type_validation(self):
+        
+    def __del__(self):
+        print('Game over')
+    
+    def data_validator(self) -> bool:
         if self.input_value.isdigit():
-            return True
+            self.input_value = int(self.input_value)
+            if 1 <= self.input_value <= 10:
+                return True
+            else:
+                print('Тебе же сказали, числа только от 1 до 10!')
+                return False
         else:
             print('Ну рили? Ты не можешь цифру от чего-то другого отличить?')
             return False
-    def range_validation(self):
-        if int(self.input_value) >= 1 and int(self.input_value) <= 10:
-            return True
-        else:
-            print('Тебе же сказали, числа только от 1 до 10!')
-            return False
 
-    def min_value_validation(self):
-        if int(self.random_value) < int(self.input_value):
-            print('Число, которое мы загадали, меньше')
-            return False
-        else:
-            return True
-
-    def max_value_validation(self):
-        if int(self.random_value) > int(self.input_value):
-            print('Число, которое мы загадали, больше')
-            return False
-        else:
-            return True
-
-    def win_validation(self):
-        if int(self.random_value) == int(self.input_value):
-            print('Кто молодец? Ты молодец!')
-            return True
-
-    def basic_validation(self):
+    def start_game(self) -> None:
         self.input_value = input('Введи число от 1 до 10\n') #можно было сделать int(input), но хотелось побаловаться с валидацией
         while True:
-            if not self.type_validation():
+            if not self.data_validator():
                 break
-            if not self.range_validation():
+            if self.input_value < self.random_value:
+                print(f'{self.input_value} меньше того, что я загадал')
                 break
-            if not self.min_value_validation():
+            if self.input_value > self.random_value:
+                 print(f'{self.input_value} больше того, что я загадал')
                 break
-            if not self.max_value_validation():
-                break
-            if self.win_validation():
+            if self.input_value == self.random_value:
+                 print(f'Поздравляю, я загадал именно {self.input_value}')
                 return
-        self.basic_validation()
+        self.start_game()
 
-x = Game()
-x.initialize()
-
+Game()
 
